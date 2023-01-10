da1f41231d6dcc2b64d9a19ed48510fc974a060b
Uku Ilves <uku.ilves@ut.ee>
2023-01-10 17:09:42 +0200

    Update uart.c

---
+++ uart.c
@@ -0,0 +1,48 @@
+#define F_CPU 3333333
+#define USART0_BAUD_RATE(BAUD_RATE) ((float)(F_CPU * 64 / (16 * (float)BAUD_RATE)) + 0.5)
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+
+void USART0_init(void);
+void USART0_sendChar(char c);
+void USART0_sendString(char *str);
+
+void USART0_init(void)
+{
+    PORTA.DIR &= ~PIN1_bm;
+    PORTA.DIR |= PIN0_bm;
+    
+    USART0.BAUD = (uint16_t)USART0_BAUD_RATE(9600);
+
+    USART0.CTRLB |= USART_TXEN_bm; 
+}
+
+void USART0_sendChar(char c)
+{
+    while (!(USART0.STATUS & USART_DREIF_bm))
+    {
+        ;
+    }        
+    USART0.TXDATAL = c;
+}
+
+void USART0_sendString(char *str)
+{
+    for(size_t i = 0; i < strlen(str); i++)
+    {
+        USART0_sendChar(str[i]);
+    }
+}
+
+int main(void)
+{
+    USART0_init();
+    
+    while (1) 
+    {
+        USART0_sendString("Hello World!\r\n");
+        _delay_ms(500);
+    }
+}
