8c024936f1287628bb1a9876270c6b772adf51e3
Uku Ilves <uku.ilves@ut.ee>
2023-01-10 17:14:39 +0200

    Update uart.c

--- uart.c
+++ uart.c
@@ -1,3 +1,4 @@
+// https://github.com/MicrochipTech/TB3216_Getting_Started_with_USART
 #define F_CPU 3333333
 #define USART0_BAUD_RATE(BAUD_RATE) ((float)(F_CPU * 64 / (16 * (float)BAUD_RATE)) + 0.5)
 
