d9b0a61f056737b6597367f77c20e4cb5ea8c27b
Uku Ilves <uku.ilves@ut.ee>
2023-01-17 19:41:52 +0200

    Add send time over uart

--- rtc.c
+++ rtc.c
@@ -1,4 +1,11 @@
+
+#define F_CPU 3333333
+#define USART0_BAUD_RATE(BAUD_RATE) ((float)(F_CPU * 64 / (16 * (float)BAUD_RATE)) + 0.5)
+
 #include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include <string.h>
 
 #define RTCC_SECONDS_REG 0x01
 #define RTCC_MINUTES_REG 0x02
@@ -17,6 +24,36 @@ static uint8_t SPI0_exchangeData(uint8_t data);
 static void rtcc_write_reg(uint8_t reg, uint8_t data);
 uint8_t rtcc_read_reg(uint8_t reg);
 void rtcc_init();
+
+
+void USART0_init(void)
+{
+	PORTA.DIR &= ~PIN1_bm;
+	PORTA.DIR |= PIN0_bm;
+	
+	USART0.BAUD = (uint16_t)USART0_BAUD_RATE(9600);
+
+	USART0.CTRLB |= USART_TXEN_bm;
+}
+
+void USART0_sendChar(char c)
+{
+	while (!(USART0.STATUS & USART_DREIF_bm))
+	{
+		;
+	}
+	USART0.TXDATAL = c;
+}
+
+void USART0_sendString(char *str)
+{
+	for(size_t i = 0; i < strlen(str); i++)
+	{
+		USART0_sendChar(str[i]);
+	}
+}
+
+
 
 static void SPI0_init(void)
 {
@@ -46,9 +83,10 @@ static uint8_t SPI0_exchangeData(uint8_t data)
 
 static void rtcc_write_reg(uint8_t reg, uint8_t data)
 {
+	uint8_t temp;
 	clientSelect();//select rtcc
-	SPI0_exchangeData(reg);// send register address
-	SPI0_exchangeData(data); // send the data to write to register
+	temp = SPI0_exchangeData(reg);// send register address
+	temp = SPI0_exchangeData(data); // send the data to write to register
 	clientDeselect();// deselect the rtcc
 }
 
@@ -65,7 +103,8 @@ uint8_t rtcc_read_reg(uint8_t reg)
 }
 
 void rtcc_init(){
-	// enable oscillator and battery backup
+	
+	//sets year to jan 1 2000 clock: 00:00:00
 	rtcc_write_reg(RTCC_SECONDS_REG, 0x00);
     rtcc_write_reg(RTCC_MINUTES_REG, 0x00);
     rtcc_write_reg(RTCC_HOURS_REG, 0x00);
@@ -73,26 +112,56 @@ void rtcc_init(){
     rtcc_write_reg(RTCC_DATE_REG, 0x01);
     rtcc_write_reg(RTCC_MONTH_REG, 0x01);
     rtcc_write_reg(RTCC_YEAR_REG, 0x00);
-	//sets year to jan 1 2000 clock: 00:00:00
+	
+	// enable oscillator and battery backup
+	rtcc_write_reg(RTCC_SECONDS_REG, 128); //Write ST to 1 aka Start oscillator(bit7)
+	rtcc_write_reg(RTCC_DAY_REG, 16); // Write VBATEN to 1 aka enable battery backup to access rtcc features(bit3)
+}
+
+void get_time(uint8_t* seconds, uint8_t* minutes, uint8_t* hours, uint8_t* day, uint8_t* date, uint8_t* month, uint8_t* year)
+{
+	*seconds = rtcc_read_reg(RTCC_SECONDS_REG);
+	*minutes = rtcc_read_reg(RTCC_MINUTES_REG);
+	*hours = rtcc_read_reg(RTCC_HOURS_REG);
+	*day = rtcc_read_reg(RTCC_DAY_REG);
+	*date = rtcc_read_reg(RTCC_DATE_REG);
+	*month = rtcc_read_reg(RTCC_MONTH_REG);
+	*year = rtcc_read_reg(RTCC_YEAR_REG);
 }
 
 static void clientSelect(void)
 {
-	PORTA.OUT &= ~PIN2_bm; // Set SS pin value to LOW
+	PORTA.OUT &= ~PIN2_bm; // Set SS pin value to LOW(RTCC)
 }
 
 static void clientDeselect(void)
 {
-	PORTA.OUT |= PIN2_bm; // Set SS pin value to HIGH
+	PORTA.OUT |= PIN2_bm; // Set SS pin value to HIGH(RTCC)
+}
+
+void get_time_string(char* time_string)
+{
+	uint8_t seconds, minutes, hours, day, date, month, year;
+	get_time(&seconds, &minutes, &hours, &day, &date, &month, &year);
+	sprintf(time_string, "%02d:%02d:%02d %02d-%02d-%02d", hours, minutes, seconds, date, month, year);
+}
+
+void send_time_string_over_uart(void)
+{
+	char time_string[20];
+	get_time_string(time_string);
+    USART0_sendString(time_string);
 }
 
 int main(void)
 {
 	uint8_t data = 0;
 	SPI0_init();
+	USART0_init();
 	rtcc_init();
 	while (1)
 	{
+		send_time_string_over_uart();
 		//clientSelect();
 		//rtcc_init();
 		//SPI0_exchangeData(data);
