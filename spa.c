e3f4f9428936f517fa7b259f9f922fac01798761
Uku Ilves <uku.ilves@ut.ee>
2023-01-30 07:56:41 +0200

    Add 2 encoder and motor functionality

--- spa.c
+++ spa.c
@@ -19,26 +19,41 @@
 
 #define ENCODER_CH_1A_PIN PIN2_bm
 #define ENCODER_CH_1B_PIN PIN3_bm
+#define ENCODER_CH_2A_PIN PIN6_bm
+#define ENCODER_CH_2B_PIN PIN7_bm
 #define ANGLE_PER_COUNT 0.03
 
 volatile int16_t encoder_position = 0;
-volatile int16_t encoder_position_buf = 0;
+volatile int16_t encoder_position2 = 0;
 volatile double angle = 0;
+volatile double enc2_angle = 0;
 void encoder_init() {
+	//encoder 2
+	PORTD.PIN6CTRL |= PORT_PULLUPEN_bm;
+	PORTD.PIN7CTRL |= PORT_PULLUPEN_bm;
+	PORTD.OUT |= ENCODER_CH_2A_PIN | ENCODER_CH_2B_PIN;
+	PORTD.PIN6CTRL |= PORT_ISC_RISING_gc;
+	PORTD.PIN7CTRL |= PORT_ISC_RISING_gc;
+	
+	
+	//encoder 1
 	//PORTD.DIR |= (ENCODER_CH_1A_PIN | ENCODER_CH_1B_PIN);
-	PORTD.PIN2CTRL |= PORT_PULLUPEN_bm;
+	PORTD.PIN2CTRL |= PORT_PULLUPEN_bm; // enable pullup for encoder pins
 	PORTD.PIN3CTRL |= PORT_PULLUPEN_bm;
 	PORTD.OUT |= ENCODER_CH_1A_PIN | ENCODER_CH_1B_PIN;
-	PORTD.PIN2CTRL |= PORT_ISC_RISING_gc; // trigger interrupt on rising
+	PORTD.PIN2CTRL |= PORT_ISC_RISING_gc; // trigger interrupt on rising without writing interrupt = disabled
 	PORTD.PIN3CTRL |= PORT_ISC_RISING_gc;
 	sei();
 }
 
 uint8_t prev_state = 0;
+uint8_t enc2_prev_state = 0;
 ISR(PORTD_PORT_vect){
 
+	
 	uint8_t new_state = (PORTD.IN & 0xC)>>2;
-	encoder_position++;
+	uint8_t new_enc2_state = (PORTD.IN & 0xC0) >> 6;
+	//encoder_position++;
 	if(new_state != prev_state)//if ((new_state == 0x02) || (new_state == 0x00)) //check for encoder state 01 00 11 10
 	{
 		if ((prev_state == 0x00 && new_state == 0x01) || (prev_state = 0x03 && new_state == 0x02))
@@ -51,15 +66,44 @@ ISR(PORTD_PORT_vect){
 	{// ((new_state == 0x03) || (new_state == 0x01)){
 		encoder_position--;
 	}
+	
+	
+	
 	angle = encoder_position * 0.03;
-	encoder_position_buf = encoder_position;
+	//encoder_position2 = encoder_position;
+	
+	// encoder 2
+	if(new_enc2_state != enc2_prev_state)//if ((new_state == 0x02) || (new_state == 0x00)) //check for encoder state 01 00 11 10
+	{
+		if ((enc2_prev_state == 0x00 && new_enc2_state == 0x01) || (enc2_prev_state = 0x03 && new_enc2_state == 0x02))
+		{
+			encoder_position2++;
+		}
+
+	}
+	else if ((enc2_prev_state == 0x01 && new_enc2_state == 0x00) || (enc2_prev_state == 0x02 && new_enc2_state == 0x03))
+	{// ((new_state == 0x03) || (new_state == 0x01)){
+		encoder_position2--;
+	}
+	
+	
 	if (angle >= 45)
 	{
-		PORTD.OUT = PORTD.OUT & 0xFE;
+		PORTD.OUT = PORTD.OUT & 0xFE; // 0 bit clear
+	}
+	
+	enc2_angle = encoder_position2 * 0.03;
+	
+	if (enc2_angle >= 20)
+	{
+		PORTD.OUT = PORTD.OUT & 0xDF; // 5 bit clear
 	}
 	//prev_state = new_state;
+	//enc2_prev_state = new_enc2_state;
 	PORTD.INTFLAGS  |= 1<<2; // interrupt has been handled for pin2
 	PORTD.INTFLAGS |= 1<<3; // interrupt has been handled for pin3
+	PORTD.INTFLAGS |= 1<<7;
+	PORTD.INTFLAGS |= 1<<6;
 }
 
 
@@ -163,11 +207,11 @@ void rtcc_init(){
 	rtcc_write_reg(RTCC_MONTH_REG, 0b00000001);
 	rtcc_write_reg(RTCC_YEAR_REG, 0b00100011);
 	
-	seconds = rtcc_read_reg(RTCC_SECONDS_REG);
+	seconds = rtcc_read_reg(RTCC_SECONDS_REG); // enable ST
 	seconds |= 1<<7;
 	rtcc_write_reg(RTCC_SECONDS_REG, seconds);
 	
-	control = rtcc_read_reg(RTCC_CONTROL_REG);
+	control = rtcc_read_reg(RTCC_CONTROL_REG); // enable EXTOSC
 	control |= 1<<3;
 	rtcc_write_reg(RTCC_CONTROL_REG, control);
 
@@ -189,7 +233,7 @@ void get_time(uint8_t* seconds, uint8_t* minutes, uint8_t* hours, uint8_t* day, 
 	*year = rtcc_read_reg(RTCC_YEAR_REG);
 	
 	// BCD decode
-	*seconds = (*seconds & 0xF) + ((*seconds >> 4) & 0x7) * 10;
+	*seconds = (*seconds & 0xF) + ((*seconds >> 4) & 0x7) * 10; // add ones and tens
 	*minutes = (*minutes & 0xF) + ((*minutes >> 4) & 0x7) * 10;
 	*hours = ((*hours >> 4) & 0x03) * 10 + (*hours & 0b1111);
 	*date = ( (*date & 0xF) + ((*date >> 4) * 10) );
@@ -235,7 +279,8 @@ int main(void)
 	//PORTD.DIR |= PIN6_bm; // Motor 2 Encoder A
 	//PORTD.DIR |= ~PIN7_bm; // Motor 2 Encoder B
 	//PORTD.OUT = 0b00010000;
-	PORTD.OUT |= 0b00000001;
+	PORTD.OUT |= 0b00000001; // left right motor
+	PORTD.OUT |= 0b00100000; // up down motor 
 	//
 	//sei();
 	while (1)
